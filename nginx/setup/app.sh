dc5cd12010a22b493676687e913806c42e1d8072
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2023-07-24 23:34:38 -0400

    fix small issues

--- nginx/setup/app.sh
+++ nginx/setup/app.sh
@@ -4,12 +4,16 @@
 #   - First parameter should be "http" OR "https"
 #   - Second parameter is the server domain
 ##
+set -ex
 PROJECT_PATH=$(echo $( cd "$( dirname "${BASH_SOURCE[0]}")" && cd .. && pwd ))
 cd $PROJECT_PATH
 
 touch .env
 export $(cat .env)
+export NGINX_PROTOCOL=${1:-http}
+
 
 # start the web server with just http
 docker-compose rm --force --stop -v  app nginx
 docker-compose up --force-recreate -d app nginx
+set +ex
--- nginx/setup/run.sh
+++ nginx/setup/run.sh
@@ -10,30 +10,36 @@ export $(cat .env)
 HOST_IP=$(ip addr show dev wlp2s0 | grep -w inet | awk '{print $2}' | cut -d '/' -f 1)
 
 # start the web server with just http
-./setup/app.sh http ${SERVER_DOMAIN}
+./setup/app.sh http
 
 # Create the first certificates for the web server
 CERTBOT_EXTRA_ARGS=
-# if [[ "$ENV" == "staging" ]]; then
-# CERTBOT_EXTRA_ARGS="--staging"
-# fi
+if [[ "$ENV" == "staging" ]]; then
+  CERTBOT_EXTRA_ARGS="--staging"
+else
+  CERTBOT_EXTRA_ARGS="--server http://$HOST_IP:4001/directory"
+fi
+
+if [[ "$ENV" == "dev" ]]; then
+  CERTBOT_EXTRA_ARGS="${CERTBOT_EXTRA_ARGS} --dry-run -vvv"
+fi
 
 set -x
 # certbot certonly --nginx --staging -d example.org
 docker-compose run --rm certbot \
   certonly -v $CERTBOT_EXTRA_ARGS \
   --webroot \
-  --webroot-path /var/www/certbot/ \
+  --webroot-path /var/www/acme-challenge/ \
   --agree-tos \
+  --no-eff-email \
+  --reinstall \
   --renew-by-default \
-  --preferred-challenges http-01 \
-  --server "http://$HOST_IP:4001/directory" \
   --text \
   --email ivan.ogasawara@gmail.com \
   -d ${SERVER_DOMAIN}
 set +x
 
 # start the web server with just https
-./setup/app.sh https ${SERVER_DOMAIN}
+./setup/app.sh https
 
 set +e
